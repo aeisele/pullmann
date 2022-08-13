818eb95d7736319c4e526f29fe98cee8c6ed0c89
Andreas Eisele <ae@andreaseisele.com>
2022-08-13 16:15:29 +0200

    initial scaffold

---
+++ src/main/java/com/andreaseisele/pullmann/PullmannApplication.java
@@ -0,0 +1,13 @@
+package com.andreaseisele.pullmann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PullmannApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PullmannApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andreaseisele/pullmann/PullmannApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreaseisele.pullmann;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PullmannApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
