d78de0dd48c00821e941ec4244478f08e74746eb
Andreas Eisele <ae@andreaseisele.com>
2022-08-15 20:55:37 +0200

    bootstrap + thymeleaf + font awesome

---
+++ src/main/java/com/andreaseisele/pullmann/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.andreaseisele.pullmann.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain secFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeRequests()
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+            .antMatchers("/webfonts/*").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin().loginPage("/login").permitAll()
+            .and()
+            .logout().permitAll()
+            .and().build();
+    }
+
+}
